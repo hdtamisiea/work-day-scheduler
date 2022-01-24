fe3b0e5dfe1d10c7314d2bd8d3f74a95b7a614b4
htam <hdfair@gmail.com>
2022-01-24 11:52:47 -0500

    added date time to top

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var date = moment().format('dddd', 'MMMM Do YYYY');
+var timeNow = moment().format('h:mm:ss a');
+
+var Workday = [
+    { time: "9 AM", event: "" },
+    { time: "10 AM", event: "" },
+    { time: "11 AM", event: "" },
+    { time: "12 PM", event: "" },
+    { time: "1 PM", event: "" },
+    { time: "2 PM", event: "" },
+    { time: "3 PM", event: "" },
+    { time: "4 PM", event: "" },
+    { time: "5 PM", event: "" },
+]
+
+$("#currentDay").text("Current date and time: " + date + timeNow);
