bc4f21e92ad51bcf23c905a2fc4892736b45748d
htam <hdfair@gmail.com>
2022-01-24 18:15:11 -0500

    function to set color of hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var date = moment().format('dddd', 'MMMM Do YYYY');
+var date = moment().format('dddd,   MMMM Do, YYYY  ');
 var timeNow = moment().format('h:mm:ss a');
 
 var hourList = [
@@ -15,4 +15,18 @@ var hourList = [
 
 $("#currentDay").text("Current date and time: " + date + timeNow);
 
-// 
+// Set up a row for each hour. hourList is variable list. hourLabel is the time displayed to the left of each hourRow
+
+
+// Set up color of row using classes provided in starter code CSS
+function setColor(time) {
+    var currentTask = moment(timeNow, "H A");
+    var tenseAssigned = moment (time, "H A");
+    if (currentTask.isBefore(tenseAssigned) === true) {
+        return "future";
+    } else if (currentTask.isAfter(tenseAssigned) === true) {
+        return "past";
+    }else {
+        return "present";
+    }
+}
